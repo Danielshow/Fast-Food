0dddf56a48253d16e8476ebaa3d46ee6a9219821
Daniel Show <danielshotonwa53@gmail.com>
2018-09-02 20:30:19 +0100

    Set up Babel to convert es6 to es5

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var dan = 0;
+var array = [1, 2, 3, 4, 4, 5];
+for (var i = 0; i < array.length; i++) {
+  array[i];
+}
--- index.js
+++
@@ -1,4 +0,0 @@
-let dan= 0
-for (var i = 0; i < array.length; i++) {
-  array[i]
-}
