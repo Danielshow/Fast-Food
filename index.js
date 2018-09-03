c2ec263d3fa4816204ce66f4d795104f34317b40
Daniel Show <danielshotonwa53@gmail.com>
2018-09-03 20:47:52 +0100

    Fix Lint errors with --fix

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const app = express();
 // use routes folder
 app.use('/api/v1', require('./routes/api'));
 
-const me = {
-  me: 'yes',
-  dan: 'im',
-};
-
-app.get('/', (req, res) => {
-  res.send(me);
-});
-
 app.listen(port, () => {
   console.log(`Server Listen on port ${port}`);
 });
