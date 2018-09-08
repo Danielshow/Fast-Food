64d0b35a2d382c025262eccc8a7cf474b1895f8f
Daniel Show <danielshotonwa53@gmail.com>
2018-09-08 01:02:24 +0100

    Set up Master

--- test/appTest.js
+++
@@ -1,8 +0,0 @@
-const assert = require('chai').assert;
-const app = require('../src/main');
-
-describe('App', () => {
-  it('app shoud return hello', ()=>{
-    assert.equal(app(), 'Hello')
-  })
-});
