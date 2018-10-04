b29e05a200efd5cd1f6ca453a1ebaee3264e9952
Daniel Show <danielshotonwa53@gmail.com>
2018-10-04 10:18:49 +0100

    Add admin route and update test case

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,6 @@ const router = Router();
 
 router.post('/auth/signup', [body.verifyBody, body.validate, body.isEmailExist], AuthController.register);
 router.post('/auth/login', [body.verifySignin, body.isEmailInDb], AuthController.login);
-router.post('/auth/login/admin', checkAuth.verifyAdminToken, [body.verifySignin, body.isEmailInDb], AuthController.adminRegister);
+router.post('/auth/signup/admin', checkAuth.verifyAdminToken, [body.verifyBody, body.validate, body.isEmailExist], AuthController.adminRegister);
 
 export default router;
