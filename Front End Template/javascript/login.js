70539ebb4ac1e1749afa45e8fdefb10dd2b0f276
Daniel Show <danielshotonwa53@gmail.com>
2018-10-09 16:57:18 +0100

    Post data to API using fetch

--- Front End Template/javascript/login.js
+++ Front End Template/javascript/login.js
@@ -1,12 +1,16 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('psw-repeat');
+const name = document.getElementById('name');
+const address = document.getElementById('address');
 const submit = document.getElementById('submit');
 const error = document.getElementById('error');
 const error2 = document.getElementById('error2');
 const loginPassword = document.getElementById('loginPassword');
 const loginEmail = document.getElementById('loginEmail');
 const loginSubmit = document.getElementById('loginSubmit');
+const url = 'http://localhost:3000/api/v1/';
+
 
 const validateEmail = ((emailInput) => {
   const re = /\S+@\S+\.\S+/;
@@ -15,7 +19,7 @@ const validateEmail = ((emailInput) => {
 
 const register = ((e) => {
   e.preventDefault();
-  if (!validateEmail(email.value)) {
+  if (!validateEmail(email.value.trim())) {
     error.innerText = 'Enter a valid email';
     return;
   } if (password.value.length < 6) {
@@ -24,8 +28,33 @@ const register = ((e) => {
   } if (password.value !== confirmPassword.value) {
     error.innerText = 'Password not match';
     return;
+  } if (address.value.trim().length < 6) {
+    error.innerText = 'Incorrect address';
+    return;
+  } if (name.value.trim().length < 1) {
+    error.innerText = 'Name cannot be empty';
+    return;
   }
-  window.location.replace('./profile.html');
+  fetch(`${url}auth/signup`, {
+    mode: 'no-cors',
+    method: 'POST',
+    body: JSON.stringify({
+      name: name.value.trim(),
+      password: password.value,
+      confirmpassword: confirmPassword.value,
+      address: address.value,
+      email: email.value,
+    }),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded, text/plain, */*; charset=utf-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'HEAD, GET, POST. PUT, PATCH, DELETE',
+      'Access-Control-Allow-headers': 'Origin, Content-Type, X-Auth-Token',
+    },
+  }).then(response => response.json()).then((data) => {
+    console.log(data);
+  });
+  // window.location.replace('./profile.html');
 });
 
 const login = ((e) => {
