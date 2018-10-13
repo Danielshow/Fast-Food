d83a37eec589c201dd317632850033cc9c78596c
Daniel Show <danielshotonwa53@gmail.com>
2018-10-13 19:43:28 +0100

    Get food ordered into alert box

--- Front End Template/javascript/order.js
+++ Front End Template/javascript/order.js
@@ -6,10 +6,17 @@ const dialogbody = document.getElementById('dialogbody');
 const dialogfooter = document.getElementById('dialogfooter');
 const dialogoverlay = document.getElementById('dialogoverlay');
 const dialogbox = document.getElementById('dialogbox');
+const placeOrder = document.getElementById('placeOrder');
 
 const closeModal = (() => {
   dialogoverlay.style.display = 'none';
   dialogbox.style.display = 'none';
+});
+
+const closeModalClear = (() => {
+  dialogoverlay.style.display = 'none';
+  dialogbox.style.display = 'none';
+  dialogbody.innerHTML = '';
 });
 
 /* eslint-disable class-methods-use-this */
@@ -19,35 +26,63 @@ class MyAlert {
     dialogbox.style.display = 'block';
     dialoghead.innerText = 'Success';
     dialogbody.innerHTML = '<img src="./images/icons/warning.png" alt="success" id="icons"><br>';
+    dialogbody.innerHTML = '';
     dialogbody.innerHTML += body;
     dialogfooter.innerHTML = '<button class = \'close\' id = \'closebutton\'> Close </button>';
     const closebutton = document.getElementById('closebutton');
     closebutton.addEventListener('click', closeModal);
   }
+
+  confirm(body) {
+    dialogoverlay.style.display = 'block';
+    dialogbox.style.display = 'block';
+    dialogbox.style.top = '1%';
+    dialoghead.innerText = 'Place Order';
+    for (let i = 0; i < body.length; i += 1) {
+      dialogbody.innerHTML += `<li>${body[i]}</li>`;
+    }
+    dialogfooter.innerHTML = '<button class = \'open\' id = \'confirmbutton\'> Order </button> <button class = \'close\' id = \'closebutton\'> Close </button>';
+    const closebutton = document.getElementById('closebutton');
+    closebutton.addEventListener('click', closeModalClear);
+  }
 }
 
 const customAlert = new MyAlert();
 
-let obj = [];
+let foodObject = [];
 const getFoodsFromClick = ((e) => {
   if (e.target.parentNode && e.target.parentNode.nodeName === 'H4') {
     const [food, price] = e.target.parentNode.innerText.split('\n');
-    obj.push(food);
-    const enw = new Set(obj);
-    if (obj.length === enw.size) {
+    foodObject.push(food.trim());
+    const uniqueFood = new Set(foodObject);
+    if (foodObject.length === uniqueFood.size) {
       foodItems.innerHTML += `<li> ${food} ${price} <button href="#" id="delete"> Delete </button> <br></li>`;
     } else {
       customAlert.alert('This food is already in your cart');
-      obj = [...enw];
+      foodObject = [...uniqueFood];
     }
   } else if (e.target.parentNode && e.target.parentNode.nodeName === 'LI') {
     let foodList = orderFood.getElementsByTagName('li');
+    // remove food from list
+    const foodToRemove = e.target.parentNode.innerText.split('$')[0].trim();
+    const newFood = foodObject.filter(x => x !== foodToRemove);
+    foodObject = newFood;
     foodList = Array.from(foodList).filter(x => x !== e.target.parentNode);
     foodItems.innerHTML = '';
     for (let i = 0; i < foodList.length; i += 1) {
       foodItems.innerHTML += `<li> ${foodList[i].innerHTML} </li>`;
     }
   }
+});
+
+const orderFoodClick = (() => {
+  const allFoods = [];
+  let foods = foodItems.getElementsByTagName('li');
+  foods = Array.from(foods);
+  for (let i = 0; i < foods.length; i += 1) {
+    allFoods.push(foods[i].innerText.split('Delete')[0].trim());
+  }
+  customAlert.confirm(allFoods);
 });
 
 const searchFunction = (() => {
@@ -65,6 +100,7 @@ const searchFunction = (() => {
 });
 
 input.addEventListener('keyup', searchFunction);
+placeOrder.addEventListener('click', orderFoodClick);
 if (document.addEventListener) {
   document.addEventListener('click', getFoodsFromClick);
 }
