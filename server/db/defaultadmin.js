11148d94135311948f6fe52cecedd8c5aca7bffd
Daniel Show <danielshotonwa53@gmail.com>
2018-10-14 23:15:33 +0100

    change default admin password

--- server/db/defaultadmin.js
+++ server/db/defaultadmin.js
@@ -1,8 +1,8 @@
 import bcrypt from 'bcryptjs';
 import db from './index';
 
-const password = bcrypt.hashSync('admin', 10);
-const params = ['admin', 'admin@foodfast.com', password, 'Default address', 'admin'];
+const password = bcrypt.hashSync('admin1', 10);
+const params = ['admin', 'admin@fastfood.com', password, 'Default address', 'admin'];
 db.query('INSERT INTO users(name, email, password, address, roles) VALUES($1,$2,$3,$4,$5)', params, (err) => {
   if (err) {
     console.log(err);
