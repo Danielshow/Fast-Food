eb72c873b4af9e57f6380ade808010c674cc2910
Daniel Show <danielshotonwa53@gmail.com>
2018-10-15 20:03:37 +0100

    Shrink spaces between form input in login page

--- server/db/table.js
+++ server/db/table.js
@@ -1,29 +1,5 @@
 import db from './index';
 
-db.query(`create table orders(
-id serial PRIMARY KEY,
-food text NOT NULL,
-quantity text NOT NULL,
-price numeric NOT NULL,
-user_id integer NOT NULL,
-status status NOT NULL
-)`, (err) => {
-  if (err) {
-    console.log(err);
-  }
-});
-
-db.query(`CREATE TABLE foodlist(
-id serial PRIMARY KEY,
-food text NOT NULL,
-price numeric NOT NULL,
-image text NOT NULL
-)`, (err) => {
-  if (err) {
-    console.log(err);
-  }
-});
-
 db.query(`CREATE TABLE users(
 id serial PRIMARY KEY,
 name text NOT NULL,
@@ -31,6 +7,29 @@ email text NOT NULL,
 password text NOT NULL,
 address text NOT NULL,
 roles roles NOT NULL
+)`, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  db.query(`create table orders(
+  id serial PRIMARY KEY,
+  food text NOT NULL,
+  quantity text NOT NULL,
+  price numeric NOT NULL,
+  user_id integer References users(id) on DELETE CASCADE,
+  status status NOT NULL
+  )`, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
+db.query(`CREATE TABLE foodlist(
+id serial PRIMARY KEY,
+food text NOT NULL,
+price numeric NOT NULL,
+image text NOT NULL
 )`, (err) => {
   if (err) {
     console.log(err);
