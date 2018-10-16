da540af80147bf25968c3e40c32792c599fa821a
Daniel Show <danielshotonwa53@gmail.com>
2018-10-16 07:41:18 +0100

    Remove unuseful route, admin register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -23,26 +23,6 @@ class AuthController {
     });
   }
 
-  adminRegister(req, res, next) {
-    const password = bcrypt.hashSync(req.body.password, 10);
-    const params = [req.body.name.trim().toLowerCase(), req.body.email.trim().toLowerCase(), password, req.body.address.trim().toLowerCase(), 'admin'];
-    db.query('INSERT INTO users(name, email, password, address, roles) VALUES($1,$2,$3,$4,$5)', params, (err) => {
-      if (err) {
-        return next(err);
-      }
-      return res.status(200).json({
-        TYPE: 'POST',
-        status: 200,
-        data: {
-          name: req.body.name.trim(),
-          email: req.body.email.trim(),
-          address: req.body.address.trim(),
-        },
-        message: 'Registered Successfully',
-      });
-    });
-  }
-
   login(req, res, next) {
     db.query('SELECT * from users WHERE email=$1', [req.body.email.toLowerCase()], (err, data) => {
       if (err) {
@@ -70,7 +50,7 @@ class AuthController {
         return res.status(403).json({
           TYPE: 'POST',
           status: 403,
-          message: 'Invalid Credentials',
+          message: 'Incorrect email or password',
         });
       }
     });
