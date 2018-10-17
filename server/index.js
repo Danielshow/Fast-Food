961a19dc0afa297b71760ac04af476731d84db4c
Daniel Show <danielshotonwa53@gmail.com>
2018-10-17 23:54:55 +0100

    Change folder name

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/uploads', express.static('uploads'));
-app.use(express.static('Front End Template'));
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
